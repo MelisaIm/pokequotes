dad979e68321a7eb94b0ecf1b94dfc28f27d1d82
Melisa Im <melisaevelynim@gmail.com>
2017-08-07 10:13:12 -0700

    Added previous javascript to pokeQuotes

--- skeleton/src/index.js
+++
@@ -1,7 +0,0 @@
-addEventListener('DOMContentLoaded', main);
-
-function main() {
-  const $root = document.querySelector('#root');
-  const $app = null; // <== Your dynamically generated DOM element goes here
-  $root.appendChild($app);
-}
